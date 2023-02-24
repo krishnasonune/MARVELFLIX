2e4ff9b0cf3927057fdd7664a6bfbe7cdea87236
krishna sonune <83691070+krishnasonune@users.noreply.github.com>
2023-02-24 19:20:58 +0530

    Add files via upload

---
+++ Entityframework/Models/Custom.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+
+namespace Entityframework.Models
+{
+    public partial class Custom
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
