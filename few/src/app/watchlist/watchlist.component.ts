be65b3d4d89fa4e09cd502da58658ce1fddfa4a9
krishna sonune <83691070+krishnasonune@users.noreply.github.com>
2023-02-24 19:28:20 +0530

    Update watchlist.component.ts

--- few/src/app/watchlist/watchlist.component.ts
+++ few/src/app/watchlist/watchlist.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ExchService } from '../dataexchange/exch.service';
 import { ServiceService } from '../sd/sc.service';
 
 @Component({
@@ -8,14 +10,27 @@ import { ServiceService } from '../sd/sc.service';
 })
 export class WatchlistComponent implements OnInit {
 
-  constructor(private service : ServiceService) { }
+  constructor(private service : ServiceService, private route : Router, private data: ExchService) { }
   email?: string;
   resp?: any;
   err?: any;
   showMsgDiv: boolean = false;
   list?: string[];
+
+  remove(r : string){
+    this.email = String(sessionStorage.getItem('user'))
+    this.service.removewlis(r, this.email).subscribe(
+      y => {
+        this.getlis();
+      },
+
+      err => {
+        console.log(err);
+      }
+    )
+  }
   
-  ngOnInit(): void {
+  getlis(){
     this.email = String(sessionStorage.getItem('user'));
     
     this.service.getwlis(this.email).subscribe(
@@ -35,6 +50,15 @@ export class WatchlistComponent implements OnInit {
         this.err = err;
       }
     )
+  }
+
+  watch(r : string){
+    this.data.setData(r);
+    this.route.navigate(['/movieplayer'])
+  }
+
+  ngOnInit(): void {
+    this.getlis();
   }
 
 }
